2e88d3693e80cfb956b50c0770634e38325ee5b4
othmanera <othmanera3@gmail.com>
2023-09-13 05:22:42 +0100

    object tracking and counting added

--- Models/Yolo detection.py
+++ Models/Yolo detection.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from ultralytics import YOLO
 import cvzone
 import math
@@ -12,7 +13,7 @@ model = YOLO("YoloWeights/yolov8l.pt")
 # #setting height/width of the webcam
 # cap.set(3,1280)
 # cap.set(4,720)
-cap = cv2.VideoCapture("../Data/videos/ApplesV2.mp4")  # video
+cap = cv2.VideoCapture("../Data/videos/ApplesVideo.mp4")  # video
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light","fire hydrant",
               "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra",
@@ -23,9 +24,18 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "cell phone", "microwave", "oven","toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
               "hair driep", "toothbrush"]
 
+#tracking
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+activeIds = []  # a list to store the ids of the current (ONLY) objects being tracked
+
+totalCount = 0  # A counter that keeps track of the number of the objects tracked
+
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
+
+    detections = np.empty((0, 5))
 
     for r in results:
         boxes = r.boxes
@@ -35,13 +45,39 @@ while True:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
             w, h = x2 - x1, y2 - y1
-            cvzone.cornerRect(img, (x1, y1, w, h))
+            #cvzone.cornerRect(img, (x1, y1, w, h))
 
             conf = math.ceil((box.conf[0] * 100) / 100)
 
             cls = int(box.cls[0])
+            currentClass = classNames[cls]
 
-            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(0, y1)))
+            if currentClass == "apple":
+                #cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(0, y1)))
+                currentArray= np.array([x1,y1,x2,y2,conf])
+                detections = np.vstack((detections,currentArray))
+
+
+
+    trackerResults = tracker.update(detections) #updates the current objects being tracked
+
+
+
+    for result in trackerResults:
+        x1,y1,x2,y2 , id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        w, h = x2 - x1, y2 - y1
+
+
+        if id not in activeIds:
+            activeIds.append(id)
+            totalCount+=1
+
+        print(activeIds)
+        cvzone.putTextRect(img, f'{int(id)}', (max(0, x1), max(0, y1)))
+        cvzone.cornerRect(img, (x1, y1, w, h))
+
+    cvzone.putTextRect(img, f'{totalCount}', (50, 50))
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
